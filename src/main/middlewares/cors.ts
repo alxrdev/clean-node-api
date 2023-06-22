687744d7404b4036ad589dcc4c9ddb3cba5871c7
Alex Rodrigues <rodriguesalex793@gmail.com>
2023-06-22 10:41:48 -0300

    feat: ensure cors is enable

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import { type NextFunction, type Request, type Response } from 'express'
+
+export const cors = (request: Request, response: Response, next: NextFunction): void => {
+  response.set('access-control-allow-origin', '*')
+  response.set('access-control-allow-methods', '*')
+  response.set('access-control-allow-headers', '*')
+  next()
+}
