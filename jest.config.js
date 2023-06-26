78a93db060dba042379a4513239788e54ca0308c
Alex Rodrigues <rodriguesalex793@gmail.com>
2023-06-26 10:54:42 -0300

    feat: ensusre mongodb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   // collectCoverage: true,
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
